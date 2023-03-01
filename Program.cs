6709e5582589c1f2161bb1d0b3395d0e5dbcb6b5
Ozan <oznkucuk@gmail.com>
2023-03-01 09:30:50 +0300

    Girilen sayi kac basamakli
    girilen sayinin kac basamakli oldugunu bulan console uygulamasi

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace girilen_sayi_kac_basamakli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // Girilen sayının kaç basamaklı olduğunu bulan program
+            Console.Write("bir sayı girin: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int basamak = 0;
+            while (sayi > 0)
+            {
+                basamak++;
+                sayi = sayi / 10;
+            }
+            Console.WriteLine("girdiğiniz sayı " + basamak.ToString() + " basamaklıdır");
+        }
+    }
+}
